71e92fc0dc103e1c3950f7b8416f995797b44220
wan <conut22@naver.com>
2025-12-13 08:27:11 +0900

    feat(fe): 필요한 엔트리 파일 추가

---
+++ fe/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './app/page';
+import './app/globals.css';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    
+      <Home />
+    
+  );
+}
